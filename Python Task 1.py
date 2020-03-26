c55c97904f999cd74ab8b9d98fd83c958bb32a97
Amaka33 <61963700+Amaka33@users.noreply.github.com>
2020-03-26 21:43:23 +0100

    Add files via upload

---
+++ Python Task 1.py
@@ -0,0 +1,5 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Radius = int(input('what is the radius of the circle (m)? '))
+Area = 22 / 7 * (Radius)**2
+print('The area of the circle = {:.2f}m\u00b2'. format(Area))
